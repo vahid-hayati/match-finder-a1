2bf33bf4f7d392446e6b711b8baba631f2a7cec3
realpj10101 <parsajfr1385oo@gmail.com>
2026-01-27 13:07:14 +0330

    add validations to register dto

--- backend/api/DTOs/RegisterDto.cs
+++ backend/api/DTOs/RegisterDto.cs
@@ -1,10 +1,13 @@
 namespace api.DTOs;
 
 public record RegisterDto(
+    [Length(1, 30)] 
     string UserName,
-    [EmailAddress]
+    [MaxLength(50), RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$", ErrorMessage = "Bad Email Format.")]
     string Email,
+    [Range(typeof(DateOnly), "1900-01-01", "2050-01-01")]
     DateOnly DateOfBirth,
+    [DataType(DataType.Password), Length(8, 16, ErrorMessage = "Min of 7 and max of 20 chars are required")] 
     string Password,
     string ConfirmPassword,
     [Length(3, 20)] string Gender
