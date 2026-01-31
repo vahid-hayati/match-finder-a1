1f7778dc572f8f64e3e52188742cb2f4e184865b
realpj10101 <parsajfr1385oo@gmail.com>
2026-01-31 12:29:09 +0330

    create get http params and use member params in member service

--- client/src/app/services/member.service.ts
+++ client/src/app/services/member.service.ts
@@ -1,11 +1,12 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Member } from '../models/member.model';
-import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
-import { PaginationParams } from '../models/helpers/paginationParams.model';
-import { PaginatedResult } from '../models/helpers/paginatedResult';
-import { PaginationHandler } from '../extensions/paginationHandler';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {inject, Injectable} from '@angular/core';
+import {Member} from '../models/member.model';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment.development';
+import {PaginationParams} from '../models/helpers/paginationParams.model';
+import {PaginatedResult} from '../models/helpers/paginatedResult';
+import {PaginationHandler} from '../extensions/paginationHandler';
+import {MemberParams} from '../models/helpers/member-params';
 
 @Injectable({
   providedIn: 'root'
@@ -16,16 +17,37 @@ export class MemberService {
   private readonly _baseApiUrl: string = environment.apiUrl + 'api/member/';
   private _paginationHandler = new PaginationHandler();
 
-  getAllMembers(paginationParams: PaginationParams): Observable<PaginatedResult<Member[]>> {
-    let params = new HttpParams();
+  getAllMembers(memberParams: MemberParams): Observable<PaginatedResult<Member[]>> {
+    const params = this.getHttpParams(memberParams)
 
-    params = params.append('pageSize', paginationParams.pageSize);
-    params = params.append('pageNumber', paginationParams.pageNumber);
+    /**
+     *       params = params.append('search', memberParams.search);
+     *       params = params.append('pageSize', memberParams.pageSize);
+     *       params = params.append('pageNumber', memberParams.pageNumber);
+     *       params = params.append('orderBy', memberParams.orderBy);
+     *       params = params.append('minAge', memberParams.minAge);
+     *       params = params.append('maxAge', memberParams.maxAge);
+     */
 
     return this._paginationHandler.getPaginatedResult<Member[]>(this._baseApiUrl + 'get-all', params);
   }
 
   getByUserName(userNameInput: string): Observable<Member | undefined> {
     return this.http.get<Member>(this._baseApiUrl + 'get-by-username/' + userNameInput);
+  }
+
+  private getHttpParams(memberParams: MemberParams): HttpParams {
+    let params = new HttpParams();
+
+    if (memberParams) {
+      params = params.append('search', memberParams.search);
+      params = params.append('pageSize', memberParams.pageSize);
+      params = params.append('pageNumber', memberParams.pageNumber);
+      params = params.append('orderBy', memberParams.orderBy);
+      params = params.append('minAge', memberParams.minAge);
+      params = params.append('maxAge', memberParams.maxAge);
+    }
+
+    return params;
   }
 }
