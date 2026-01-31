4e37973fc5705bface2f954abe8ca955a16725b6
realpj10101 <parsajfr1385oo@gmail.com>
2026-01-31 12:28:37 +0330

    move params to create query method

--- backend/api/Repositories/MemberRepository.cs
+++ backend/api/Repositories/MemberRepository.cs
@@ -15,36 +15,8 @@ public class MemberRepository : IMemberRepository
 
     public async Task<PagedList<AppUser>> GetAllAsync(MemberParams memberParams, CancellationToken cancellationToken)
     {
-        DateOnly minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MaxAge -1));
-        DateOnly maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MinAge));
-        
-        Console.WriteLine(minDob);
-        Console.WriteLine(maxDob);
-        
-        IQueryable<AppUser> query = _collection.AsQueryable();
-
-        query = memberParams.OrderBy switch
-        {
-            "age" => query.OrderBy(appUser => appUser.DateOfBirth).ThenBy(appUser => appUser.Id),
-            "created" => query.OrderByDescending(appUser => appUser.CreatedOn).ThenBy(appUser => appUser.Id),
-            _ => query.OrderByDescending(appUser => appUser.LastActive).ThenBy(appUser => appUser.Id)
-        };
-        
-        if (!string.IsNullOrEmpty(memberParams.Search))
-        {
-            query = query.Where(
-                u => u.NormalizedUserName.Contains(memberParams.Search, StringComparison.CurrentCultureIgnoreCase) // "rezab"  , san// "reza taba"
-                     || u.City.Contains(memberParams.Search, StringComparison.CurrentCultureIgnoreCase)
-                     || u.Country.Contains(memberParams.Search, StringComparison.CurrentCultureIgnoreCase)
-            );
-        }
-        
-        query = query.Where(u => !(u.NormalizedUserName.Equals("ADMIN") || u.NormalizedUserName.Equals("MODERATOR")));
-        query = query.Where(u => u.Id.ToString() != memberParams.UserId);
-        query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
-
         PagedList<AppUser> appUsers = await PagedList<AppUser>.CreatePagedListAsync(
-            query, memberParams.PageNumber, memberParams.PageSize, cancellationToken
+            CreateQuery(memberParams), memberParams.PageNumber, memberParams.PageSize, cancellationToken
         );
 
         return appUsers;
@@ -64,40 +36,36 @@ public class MemberRepository : IMemberRepository
 
         return memberDto;
     }
-    
-    /*
-     *   private IQueryable<AppUser> CreateQuery(MemberParams memberParams)
-         {
-             // calculate DOB based on user's selected Age
-             DateOnly minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MaxAge - 1));
-             DateOnly maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MinAge));
 
-             IQueryable<AppUser> query = _collection.AsQueryable();
+    private IQueryable<AppUser> CreateQuery(MemberParams memberParams)
+    {
+        DateOnly minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MaxAge - 1));
+        DateOnly maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MinAge));
+        
+        IQueryable<AppUser> query = _collection.AsQueryable();
 
-             query = memberParams.OrderBy switch
-             {
-                 "age" => query.OrderBy(appUser => appUser.DateOfBirth).ThenBy(appUser => appUser.Id),
-                 "created" => query.OrderByDescending(appUser => appUser.CreatedOn).ThenBy(appUser => appUser.Id),
-                 _ => query.OrderByDescending(appUser => appUser.LastActive).ThenBy(appUser => appUser.Id)
-             };
+        query = memberParams.OrderBy switch
+        {
+            "age" => query.OrderBy(appUser => appUser.DateOfBirth).ThenBy(appUser => appUser.Id),
+            "created" => query.OrderByDescending(appUser => appUser.CreatedOn).ThenBy(appUser => appUser.Id),
+            _ => query.OrderByDescending(appUser => appUser.LastActive).ThenBy(appUser => appUser.Id)
+        };
+        
+        if (!string.IsNullOrEmpty(memberParams.Search))
+        {
+            memberParams.Search = memberParams.Search.ToUpper();
 
-             if (!string.IsNullOrEmpty(memberParams.Search))
-             {
-                 memberParams.Search = memberParams.Search.ToUpper();
+            query = query.Where(
+                u => u.NormalizedUserName!.Contains(memberParams.Search, StringComparison.CurrentCultureIgnoreCase) // "rezab"  , san// "reza taba"
+                     || u.City.Contains(memberParams.Search, StringComparison.CurrentCultureIgnoreCase)
+                     || u.Country.Contains(memberParams.Search, StringComparison.CurrentCultureIgnoreCase)
+            );
+        }
+        
+        query = query.Where(u => !(u.NormalizedUserName!.Equals("ADMIN") || u.NormalizedUserName == "MODERATOR"));
+        query = query.Where(u => u.Id.ToString() != memberParams.UserId);
+        query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
 
-                 query = query.Where(
-                     u => u.KnownAs.Contains(memberParams.Search, StringComparison.CurrentCultureIgnoreCase)
-                          || u.NormalizedUserName.Contains(memberParams.Search, StringComparison.CurrentCultureIgnoreCase) // "rezab"  , san// "reza taba"
-                          || u.City.Contains(memberParams.Search, StringComparison.CurrentCultureIgnoreCase)
-                          || u.Country.Contains(memberParams.Search, StringComparison.CurrentCultureIgnoreCase)
-                 );
-             }
-
-             query = query.Where(u => !(u.NormalizedUserName!.Equals("ADMIN") || u.NormalizedUserName == "MODERATOR"));
-             query = query.Where(u => u.Id != memberParams.UserId);
-             query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
-
-             return query;
-         }
-     */
+        return query;
+    }
 }
