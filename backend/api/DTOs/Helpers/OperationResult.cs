076ceba566426d20a62e746e3af000bb35a6eed5
vahid-hayati <vahidhayati68@gmail.com>
2026-02-03 20:03:15 +0330

    create operation result

---
+++ backend/api/DTOs/Helpers/OperationResult.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+
+namespace api.DTOs.Helpers;
+
+public record OperationResult<T>(
+    bool IsSuccess,
+    [Optional] T Result,
+    CustomError? Error
+);
+
+public record OperationResult(
+    bool IsSuccess,
+    CustomError? Error
+);
+
+public record CustomError(
+    Enum Code,
+    [Optional] string? Message
+);
